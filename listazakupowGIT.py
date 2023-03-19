cdfe899fa44ecbc5fc520c507cb0f2404d075dd5
Michal <kusmichal.ku@gmail.com>
2023-03-19 15:47:53 +0100

    end_projekt

--- listazakupowGIT.py
+++ listazakupowGIT.py
@@ -5,3 +5,5 @@ shop = {
 print("Lista zakupów")
 for items in shop:
     print(f'Idę do {items.upper()} kupuję tu: {shop[items]}')
+x=len(shop["rowerowy"])+ len(shop["decathlon"])
+print(f'W Sumie kupiłem {x} produktów')
